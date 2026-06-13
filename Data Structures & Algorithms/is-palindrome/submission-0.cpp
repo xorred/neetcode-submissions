2af60440d02f04b786441c184597ef680ddf701b
Sagar <sagarspawar1511@gmail.com>
2026-06-13 20:07:00 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // string n;
+        // for(int i=0; i<s.size();i++){
+        //     // if
+        //     n[i] = tolower(s[i]);
+        // }
+
+        string n = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (isalnum(s[i])) {          // Only keep letters and numbers
+                n += tolower(s[i]);       // Safely append to the end
+            }
+        }
+
+        int i = 0;
+        int j = n.size() - 1;
+        while(i<j)
+        {
+            if(n[i] != n[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+};
